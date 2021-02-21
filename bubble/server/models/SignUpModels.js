cc7386a4c41c9aa8c2169501f23e47c54f279f8f
Yuan <yuanionrings@gmail.com>
2021-02-20 23:51:10 -0500

    added express and mongodb

---
+++ bubble/server/models/SignUpModels.js
@@ -0,0 +1,27 @@
+// Sign up schema - what to collect from user
+const mongoose = require('mongoose')
+
+const signUpTemplate = new mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    date:{
+        type: Date,
+        dafault: Date.now
+    }
+})
+
+module.exports = mongoose.model('mytable', signUpTemplate)
